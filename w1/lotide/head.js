a6f0034c60ecea7005bbdbd31e2c3dd74a1c6806
reza477 <alexfitzgrld@gmail.com>
2019-08-20 00:43:15 +0000

    assertEquall version 2

--- w1/lotide/head.js
+++ w1/lotide/head.js
@@ -10,3 +10,13 @@ const assertEqual = function(actual, expected) {
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+var head = function(array){
+    // return array.head
+   return array[0]
+  // } else {return "undefined"}
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
